34dd12487d6307571cc3a7b5b41dce5764ce4fad
abdelrhman <abdelrhmanfathy1@yahoo.com>
2019-02-13 15:08:38 +0200

    just commit to check out

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.androidproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton mAddTrip = findViewById(R.id.fb_incoming_trip_add_trip);
+        mAddTrip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddTripActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
