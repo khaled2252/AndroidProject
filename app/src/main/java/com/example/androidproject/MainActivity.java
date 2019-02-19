4793eae62ced2c9b39d14cbc25eb4569bfe28385
mona <mona.ali3c@gmail.com>
2019-02-19 16:26:43 +0200

    First commit, Login and Sign up activities.

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -4,14 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button signup;
+    Button signin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this,PastTripsActivity.class));
+        //startActivity(new Intent(this,PastTripsActivity.class));
+        signup = (Button) findViewById(R.id.signupbtn);
+        signin = (Button) findViewById(R.id.signintbtn);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signup1 = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(signup1);
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login1 = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(login1);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/androidproject/SignupActivity.java
@@ -0,0 +1,180 @@
+package com.example.androidproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class SignupActivity extends Activity {
+
+    EditText email;
+    EditText password;
+    EditText confirmpass;
+    Button signup;
+    TextView backlogin;     //As Button
+    SignInButton googleSignin;
+
+    //Authentication
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    GoogleSignInClient mGoogleSignInClient;
+
+    private static final String TAG = "";
+    private final static int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        email = findViewById(R.id.emailtxt);
+        password = findViewById(R.id.passtxt);
+        confirmpass = findViewById(R.id.confirmpasstxt);
+        signup = (Button) findViewById(R.id.newaccountbtn);
+        backlogin = findViewById(R.id.backlogintxt);
+        googleSignin = (SignInButton) findViewById(R.id.sign_in_button);
+
+        backlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login2 = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(login2);
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null){
+                    //Intent user account
+                    startActivity(new Intent(SignupActivity.this, PastTripsActivity.class));
+                    finish();
+                }
+            }
+        };
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        googleSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String Email = email.getText().toString();
+                String Password = password.getText().toString();
+                String Confirm = confirmpass.getText().toString();
+
+                if (TextUtils.isEmpty(Email)) {
+                    Toast.makeText(getApplicationContext(), "Please Enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(Password)) {
+                    Toast.makeText(getApplicationContext(), "Please Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(Confirm)) {
+                    Toast.makeText(getApplicationContext(), "Please Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!TextUtils.equals(Password, Confirm)) {
+                    Toast.makeText(getApplicationContext(), "Password don't match!", Toast.LENGTH_SHORT).show();
+                    password.setText("");
+                    confirmpass.setText("");
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(Email,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignupActivity.this, "Register Successfully", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(SignupActivity.this, PastTripsActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            Toast.makeText(SignupActivity.this, "Register Failed", Toast.LENGTH_LONG).show();
+                            email.setText("");
+                            password.setText("");
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent home = new Intent(SignupActivity.this, PastTripsActivity.class);
+                            startActivity(home);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(SignupActivity.this, "Aut Fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
